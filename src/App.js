8a2dcbbc4e42f86429c151438204179259ab2131
Christopher <chrisrw300@gmail.com>
2021-02-16 18:27:04 -0700

    added component files to start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+// import component pages
+/*
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Search from './components/Search';
+import Footer from './components/Footer';
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>hello</p>
   );
 }
 
